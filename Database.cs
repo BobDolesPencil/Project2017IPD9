cd524b15849c078143a6ba061f79c360f154a2b2
Sebastien Scullion <ira.tus007@gmail.com>
2017-04-13 11:55:21 -0400

    *** class addition

---
+++ Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipdMPMain
+{
+    class Database
+    {
+        private SqlConnection conn;
+        public Database()
+        {
+            // IF MULTI  updates....use TRANSACTIONS...
+            conn = new SqlConnection("Server=tcp:mocko.database.windows.net,1433;Initial Catalog=MockO;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            conn.Open();
+        }
+
+    }
+
+    public void AddUser(NewUser nu)
+    {
+        string sql = "INSERT INTO Users.UserProfile(userID)"
+    }
+}
---
+++ NewUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipdMPMain
+{
+    class NewUser
+    {
+        public NewUser(string fname, string lname, string username, string password)
+        {
+            FirstName = fname;
+            LastName = lname;
+            UserName = username;
+            Password = password;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
